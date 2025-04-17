22222dab6fa03cd644dc7dc8f4bd9fbbf3989ba2
will <wilsonluizzzz35@gmail.com>
2025-04-17 23:41:48 +0000

    função cavalo

--- xadrezNivelMestre.c
+++ xadrezNivelMestre.c
@@ -35,6 +35,28 @@
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
+    void moverCavalo(int casas)
+{
+    for (int i = 0; i < casas; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (j == 2) 
+            // Quando j for igual a 2, é hora de mover pra direita
+            {
+                printf("O cavalo se move para a direita\n");
+                break; // termina o loop
+            }
+
+            if (j < 2) 
+            //quando j estiver menor que 2, o cavalo se move para cima, ou seja, duas vezes.
+            {
+                printf("O cavalo se move para cima\n");
+                continue; // pula o loop, incrementa o j e volta para o for
+            }
+        }
+    }
+}
 
 
 
@@ -42,6 +64,7 @@
 
 int main(){
 
+    
 
     return 0;
 }
