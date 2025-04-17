4504e984861d3e7683aa78cd3165be6193e20d09
will <wilsonluizzzz35@gmail.com>
2025-04-17 23:32:28 +0000

    novo arquivo para o desafio nivel mestre

---
+++ xadrezNivelMestre.c
@@ -0,0 +1,16 @@
+    // Nível Mestre - Funções Recursivas e Loops Aninhados
+    // Sugestão: Substitua as movimentações das peças por funções recursivas.
+    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+
+    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
+    // Inclua o uso de continue e break dentro dos loops.
+
+
+
+#include <stdio.h>
+
+int main(){
+
+
+    return 0;
+}
