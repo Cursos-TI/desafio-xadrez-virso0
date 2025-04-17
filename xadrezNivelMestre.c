5a0d19880de4f256b7356d84a99e92a599864b20
will <wilsonluizzzz35@gmail.com>
2025-04-17 23:33:03 +0000

    função da torre

--- xadrezNivelMestre.c
+++ xadrezNivelMestre.c
@@ -1,5 +1,14 @@
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
+    void moverTorre(int casas)
+{
+    if (casas > 0)
+    {
+        printf("A torre se move para a Direita\n");
+        moverTorre(casas-1);
+    }
+
+}
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
